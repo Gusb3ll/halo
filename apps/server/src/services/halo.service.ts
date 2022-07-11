d3a85bf4e88af709f6ebf2de74dff8e4844760ba
Gusb3ll <Kitpipat.0647@gmail.com>
2022-07-11 23:38:25 +0700

    refactor: reduce code size

--- apps/server/src/services/halo.service.ts
+++ apps/server/src/services/halo.service.ts
@@ -10,13 +10,8 @@ interface ICunnySauce {
 export async function getCunnySauce(): Promise<ICunnySauce | undefined> {
   const pageIndex = Math.floor(Math.random() * 1000)
   const data: ICunnySauce = await booru.posts({ tags: 'blue_archive', page: pageIndex }).then((posts) => {
-    const index = Math.floor(Math.random() * posts.length)
-    const post = posts[index]
-
-    const url: string = booru.url(post.file_url).href
-    const name: string = post.md5
-
-    return { url, name }
+    const { file_url, md5 } = posts[Math.floor(Math.random() * 20)]
+    return { url: file_url, name: md5 }
   })
 
   return data
